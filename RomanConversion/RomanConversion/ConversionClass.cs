90beca1028c0f6b2ffa8b63c8539051f29d4d098
Cat <catwhitaker@gmail.com>
2018-04-06 11:44:27 -0700

    RomanConversion
    beta version

---
+++ RomanConversion/RomanConversion/ConversionClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RomanConversion
+{
+    public class ConversionClass
+    {
+        public string decimalNum { get; set; }
+        public string romanNum { get; set; }
+
+        //constructor
+        public ConversionClass(string dN, string rN)
+        {
+            decimalNum = dN;
+            romanNum = rN;
+        }
+
+        //public override string ToString()
+        //{
+        //    return Spanish;
+        //}
+
+
+            /*
+            public toDecimal(string key, string value)
+            {
+                this.Key= key;
+                this.Value = value;
+            }  
+            */
+            public override string toRoman(int key)
+            {
+                if (convert.ContainsKey(key))
+                {
+                    string value = convert[key];
+
+                }
+                return value;
+            }
+        }
+    }
+}
---
+++ RomanConversion/RomanConversion/ViewController.designer.cs
@@ -0,0 +1,64 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace RomanConversion
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel instructionsLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel resultLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ToDecimalButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton toRomanButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField usrTextView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (instructionsLabel != null) {
+                instructionsLabel.Dispose ();
+                instructionsLabel = null;
+            }
+
+            if (resultLabel != null) {
+                resultLabel.Dispose ();
+                resultLabel = null;
+            }
+
+            if (ToDecimalButton != null) {
+                ToDecimalButton.Dispose ();
+                ToDecimalButton = null;
+            }
+
+            if (toRomanButton != null) {
+                toRomanButton.Dispose ();
+                toRomanButton = null;
+            }
+
+            if (usrTextView != null) {
+                usrTextView.Dispose ();
+                usrTextView = null;
+            }
+        }
+    }
+}
