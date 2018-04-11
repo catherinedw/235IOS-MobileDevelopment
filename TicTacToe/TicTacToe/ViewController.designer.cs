e2692408510b2ae50bbbb3a261c4a2cefa236cac
student <>
2018-04-11 14:39:09 -0700

    layout complete

--- TicTacToe/TicTacToe/ViewController.designer.cs
+++ TicTacToe/TicTacToe/ViewController.designer.cs
@@ -15,10 +15,104 @@ namespace TicTacToe
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton a1Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton a2Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton a3Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton b1Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton b2Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton b3Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton c1Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton c2Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton c3Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel instructorLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UIButton startButton { get; set; }
+
+        [Action ("UIButton402_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton402_TouchUpInside (UIKit.UIButton sender);
 
         void ReleaseDesignerOutlets ()
         {
+            if (a1Button != null) {
+                a1Button.Dispose ();
+                a1Button = null;
+            }
+
+            if (a2Button != null) {
+                a2Button.Dispose ();
+                a2Button = null;
+            }
+
+            if (a3Button != null) {
+                a3Button.Dispose ();
+                a3Button = null;
+            }
+
+            if (b1Button != null) {
+                b1Button.Dispose ();
+                b1Button = null;
+            }
+
+            if (b2Button != null) {
+                b2Button.Dispose ();
+                b2Button = null;
+            }
+
+            if (b3Button != null) {
+                b3Button.Dispose ();
+                b3Button = null;
+            }
+
+            if (c1Button != null) {
+                c1Button.Dispose ();
+                c1Button = null;
+            }
+
+            if (c2Button != null) {
+                c2Button.Dispose ();
+                c2Button = null;
+            }
+
+            if (c3Button != null) {
+                c3Button.Dispose ();
+                c3Button = null;
+            }
+
+            if (instructorLabel != null) {
+                instructorLabel.Dispose ();
+                instructorLabel = null;
+            }
+
             if (startButton != null) {
                 startButton.Dispose ();
                 startButton = null;
