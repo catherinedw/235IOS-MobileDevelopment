2043c673e1cbc608fc596160c09307a2571418ef
Cat <catwhitaker@gmail.com>
2018-04-20 12:56:15 -0700

    tax switch value change => amount change

--- TipCalculator/TipCalculator/ViewController.cs
+++ TipCalculator/TipCalculator/ViewController.cs
@@ -7,9 +7,9 @@ namespace TipCalculator
     public partial class ViewController : UIViewController
     {
         /***variables***/
-        double amount = 0.00;
+        //double amount;
         double tipAmount, taxAmount, total;
-        int tipPercent, taxPercent = 0;
+        int taxPercent = 0;
 
         protected ViewController(IntPtr handle) : base(handle)
         {
@@ -29,14 +29,15 @@ namespace TipCalculator
             // Perform any additional setup after loading the view, typically from a nib.
             //default of switch is off
             taxSwitch.On = false;
-            taxPercentageTextView.Text = taxPercent.ToString();
+            taxPercentageTextView.Text = ""; //taxPercent.ToString();
 
             //This gets rid of the text when you are done editing
+ //TODO validate input
             amountTextView.EditingDidEndOnExit += (sender, e) => {
-                tipAmount = Double.Parse(amountTextView.Text) * serviceSlider.Value / 100;
-                taxAmount = Double.Parse(amountTextView.Text) * taxPercent / 100;
+                tipAmount = Math.Round(Double.Parse(amountTextView.Text) * serviceSlider.Value / 100);
+                taxAmount = Math.Round(Double.Parse(amountTextView.Text) * taxPercent / 100);
                 //should make this a method
-                total = Double.Parse(amountTextView.Text) + tipAmount + taxAmount;
+                total = Math.Round(Double.Parse(amountTextView.Text) + tipAmount + taxAmount);
                 //should make this a method
                 tipAmountTextView.Text = tipAmount.ToString();
                 taxAmountTextView.Text = taxAmount.ToString();
@@ -46,33 +47,27 @@ namespace TipCalculator
 
             tipPercentageTextView.EditingDidEndOnExit += (sender, e) => {
                 serviceSlider.Value = Int32.Parse(tipPercentageTextView.Text); //this changes the slider value to match the input
-                tipAmount = Double.Parse(amountTextView.Text) * serviceSlider.Value / 100;
+                tipAmount = Math.Round(Double.Parse(amountTextView.Text) * serviceSlider.Value / 100);
                 tipAmountTextView.Text = tipAmount.ToString();//(Double.Parse(amountTextView.Text) * Double.Parse(tipPercentageTextView.Text) / 100).ToString();
-                total = Double.Parse(amountTextView.Text) + tipAmount + taxAmount;
+                total = Math.Round(Double.Parse(amountTextView.Text) + tipAmount + taxAmount);
                 totalTextView.Text = total.ToString();//(Double.Parse(amountTextView.Text) + Double.Parse(tipAmountTextView.Text) + Double.Parse(taxAmountTextView.Text)).ToString();
                 ((UITextField)sender).ResignFirstResponder();
             };
 
             taxPercentageTextView.EditingDidEndOnExit += (sender, e) => {
-                //TODO disable till switch on
+                //disable till switch on
                 if (taxPercentageTextView.UserInteractionEnabled)
                 {
-                    taxAmount = Double.Parse(amountTextView.Text) * Double.Parse(taxPercentageTextView.Text) / 100;
+                    taxAmount = Math.Round(Double.Parse(amountTextView.Text) * Double.Parse(taxPercentageTextView.Text) / 100);
                     taxAmountTextView.Text = taxAmount.ToString();//(Double.Parse(amountTextView.Text) * Double.Parse(tipPercentageTextView.Text) / 100).ToString();
-                    total = Double.Parse(amountTextView.Text) + tipAmount + taxAmount;
+                    total = Math.Round(Double.Parse(amountTextView.Text) + tipAmount + taxAmount);
                     totalTextView.Text = total.ToString();//(Double.Parse(amountTextView.Text) + Double.Parse(tipAmountTextView.Text) + Double.Parse(taxAmountTextView.Text)).ToString();
                 }
                     ((UITextField)sender).ResignFirstResponder();
             };
-            /*
-            ((UIControl)View).TouchDown += (sender, e) => {
-                nameField.ResignFirstResponder();
-                numberField.ResignFirstResponder();
-            };
-            */
         }
 
-        //Event 
+        //slide the slider
         partial void serviceSlider_ValueChanged(UISlider sender)
         {
             int progress = (int)sender.Value;
@@ -84,6 +79,7 @@ namespace TipCalculator
             //tipAmountTextView.Text = progress.ToString();
         }
 
+        //switch change
         partial void taxSwitch_ValueChanged(UISwitch sender)
         {
             bool setting = sender.On;
@@ -91,27 +87,33 @@ namespace TipCalculator
                 //TODO enable tax value edit
                 taxPercentageTextView.UserInteractionEnabled = true; 
             }
+            else//if (!taxSwitch.On)
+            {
+                //TODO change total amount back to what it would be without tax
+                taxPercentageTextView.UserInteractionEnabled = false;
+                taxPercent = 0;
+            }
             //rightSwitch.SetState(setting, true);
         }
 
+        //make sure you want to add tax
         partial void taxSwitch_ActionSheet(UISwitch sender)
         {
             //Conroller
             //ActionSheet
-            var controller = UIAlertController.Create(String.Format("Are You Sure You Want to Tip {0}?", tipPercentageTextView.Text), null, UIAlertControllerStyle.ActionSheet);
+            var controller = UIAlertController.Create("Are You Sure You Want to Add a Tax?", null, UIAlertControllerStyle.ActionSheet);
 
             var yesAction = UIAlertAction.Create("Yes, I'm Sure!", UIAlertActionStyle.Destructive,
                 (action) =>
                 {
-                string msg = Int32.Parse(this.tipPercentageTextView.Text) > 15
-                      ? "You can breath easy, you tipped correctly"
-                      : "Sorry you had such bad service";
+                string msg = Int32.Parse(this.taxPercentageTextView.Text) == 0
+                      ? "You can breath easy, you live in Oregon"
+                      : "Sorry you live in a State with state tax";
 
                     // Controller within a controller
-                    var cancelAction = UIAlertAction.Create("Phew, You Can Still Change Your Tip!", UIAlertActionStyle.Cancel, null);
+                    var cancelAction = UIAlertAction.Create("Phew, You Can Still Change Your Tax Percentage!", UIAlertActionStyle.Cancel, null);
 
-                //wha???????
-                    var controller2 = UIAlertController.Create("You Picked Your Tip Amount", msg, UIAlertControllerStyle.Alert);
+                var controller2 = UIAlertController.Create(String.Format("Tax Amount: {0}", taxAmount), msg, UIAlertControllerStyle.Alert);
                     controller2.AddAction(cancelAction);
                     this.PresentViewController(controller2, true, null);
                 });
