df7c88d881d5fa16311bf37af800ba55230eea76
Cat <catwhitaker@gmail.com>
2018-05-03 13:02:27 -0700

    GetItDone initial commit

---
+++ GetItDone/GetItDone/TableViewSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UIKit;
+using System.Collections.Generic;  
+using Foundation;  //this lets you inherit the UITableViewSource
+
+
+namespace GetItDone
+{
+    public class TableViewSource: UITableViewSource
+    {
+        List < string > TableData;  
+        string CellIdentifier = "TableCell";
+        //In TableSource method insert a list of string parameter.
+        public TableViewSource(List < string > items) {  
+            TableData = items;  
+        } 
+        //in RowInSection -
+        public override nint RowsInSection(UITableView tableview, nint section) {  
+            return TableData.Count;  
+        }  
+        //In GetCell method -
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath) {  
+            UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);  
+            //---- if there are no cells to reuse, create a new one
+            if (cell == null) {  
+                cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);  
+            }  
+            cell.TextLabel.Text = TableData[indexPath.Row];  
+            return cell;  
+        }  
+    }
+}
---
+++ GetItDone/GetItDone/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic; 
+using UIKit;
+
+namespace GetItDone
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            //table = new UITableView(View.Bounds); // defaults to Plain style
+            // Perform any additional setup after loading the view, typically from a nib.
+            List < string > itemData = new List < string > ()  
+            {  
+                "Android",  
+                "IOS",  
+                "Windows Phone",  
+                "Xamarin-IOS",  
+                "Xamarin-Form",  
+                "Xamarin_Android"  
+            };  
+            mainTableView.Source = new TableViewSource(itemData); 
+            //mainListview.TableFooterView = new UIView(CoreGraphics.CGRect.Empty); 
+            //table.Source = new TableViewSource(itemData); 
+            //Add(table);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+        /*cell border is not left-aligned
+        public override void WillDisplay(UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)  
+        {  
+            if (cell.RespondsToSelector(new ObjCRuntime.Selector("setSeparatorInset:"))) cell.SeparatorInset = UIEdgeInsets.Zero;  
+            if (cell.RespondsToSelector(new ObjCRuntime.Selector("setLayoutMargins:"))) cell.LayoutMargins = UIEdgeInsets.Zero;  
+        } */ 
+    }
+}
