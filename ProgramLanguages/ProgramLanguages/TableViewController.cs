ec8a00d0f5f23edb66e15ca9062cd92ade3480c7
Cat <catwhitaker@gmail.com>
2018-05-22 12:53:18 -0700

    displays table

--- ProgramLanguages/ProgramLanguages/TableViewController.cs
+++ ProgramLanguages/ProgramLanguages/TableViewController.cs
@@ -1,13 +1,81 @@
-using Foundation;
 using System;
 using UIKit;
-
 namespace ProgramLanguages
 {
     public partial class TableViewController : UITableViewController
     {
+        private string[] tableLanguages = {
+            "Javascript", "Python", "Java", "Ruby", "PHP"
+                /*
+C++
+CSS
+C#
+GO
+C
+TypeScript
+Shell
+Swift
+Scala
+Objective-C
+Rust
+Elixir
+R
+Perl
+Elm
+Kotlin
+Crystal
+Visual Basic
+Matlab
+Haskell
+
+Groovy
+SQL
+XML
+CoffeeScript
+Groovy
+Clojure
+Arduino
+Lua
+Poweshell
+XSL
+Assembly
+TypeScript
+ActionScript
+Erlang
+ASP
+Dart
+VimL
+Delhi
+OCaml
+ColdFusion
+TeX
+Makefile
+F#
+Prelog
+Cuda
+*/
+            };
         public TableViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            TableView.DataSource = new TableViewDataSource(tableLanguages);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
--- ProgramLanguages/ProgramLanguages/TableViewDataSource.cs
+++ ProgramLanguages/ProgramLanguages/TableViewDataSource.cs
@@ -1,10 +1,66 @@
 ﻿using System;
+using UIKit;
+using Foundation; // needed for dataSource inheritance
+
+
 namespace ProgramLanguages
 {
-    public class TableViewDataSource
+    public class TableViewDataSource: UITableViewDataSource
     {
-        public TableViewDataSource()
+        // there is NO database or storage of Tasks in this example, just an in-memory array
+        string[] tableItems;
+        string cellIdentifier = "LanguagesCell"; // set in the Storyboard
+
+        public TableViewDataSource(string[] items)
         {
+            tableItems = items;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return tableItems.Length;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            // in a Storyboard, Dequeue will ALWAYS return a cell,
+            UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
+            // now set the text 
+            //cell.TextLabel.Text = tableItems[indexPath.Row];
+            string item = tableItems[indexPath.Row];
+
+            // Add images to the cell
+            //cell.ImageView.Image = UIImage.FromFile("Images/star");
+            //cell.ImageView.HighlightedImage = UIImage.FromFile("Images/star2");
+
+            // Cell style should be set to "Subtitle" or "RightDetial" in the storyboard
+            // Add detail information
+            /*
+            const int NUM_DISNEY_DWARVES = 7;
+            if (cell.DetailTextLabel != null)
+            {
+                if (indexPath.Row < NUM_DISNEY_DWARVES)
+                {
+                    cell.DetailTextLabel.Text = "Mr Disney";
+                }
+                else
+                {
+                    cell.DetailTextLabel.Text = "Mr Tolkien";
+                }
+            }
+            
+            */
+            //---- if there are no cells to reuse, create a new one
+            if (cell == null)
+            { cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier); }
+
+            cell.TextLabel.Text = item;
+            return cell;
+        }
+
+        public string GetItem(int id)
+        {
+            return tableItems[id];
         }
     }
 }
