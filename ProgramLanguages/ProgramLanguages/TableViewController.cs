a9e9b8da6b2443478591cfda1b8bf74e3951a87c
Cat <catwhitaker@gmail.com>
2018-05-22 15:30:12 -0700

    with sections

--- ProgramLanguages/ProgramLanguages/TableViewController.cs
+++ ProgramLanguages/ProgramLanguages/TableViewController.cs
@@ -1,12 +1,61 @@
 using System;
 using UIKit;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ProgramLanguages
 {
     public partial class TableViewController : UITableViewController
     {
-        private string[] tableLanguages = {
+        private string[] tableLanguages;
+        //List<tableItem> tableItems = new List<tableItem>();
+        //string[] keys;  // V, F, L, B, T
+        //Dictionary<string, List<string>> indexedTableItems;
+
+
+        public TableViewController(IntPtr handle) : base(handle)
+        {
+            tableLanguages = new string[] {
             "Javascript", "Python", "Java", "Ruby", "PHP"
-                /*
+            };
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            TableView.DataSource = new TableViewDataSource(tableLanguages);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+
+};
+
+
+            /*    
+             * 
+             *         
+             private string[,] tableLanguages = new string[5, 4]{
+            //{"Javascript", "Python", "Java", "Ruby", "PHP"};
+            {"Javascript", "1995", "Brendan Eich", "ActionScript, AtScript, CoffeeScript, Dart, JScript .NET, LiveScript, Objective-J, Opa, Perl 6, QML, TypeScript"},
+            {"Python", "1990", "Guido van Rossum", "Boo, Cobra, Coconut, CoffeeScript, D, F#, Falcon, Genie, Go, Groovy, JavaScript, Julia, Nim, Ring, Ruby, Swift"},
+            {"Java", "1995", "James Gosling", "Ada 83, C++, C#, Eiffel, Generic Java, Mesa, Modula-3, Oberon, Objective-C, UCSD Pascal, Object Pascal"},
+            {"Ruby", "1995", "Yukihiro Matsumoto", "Ada,[5] C++,[5] CLU,[6] Dylan,[6] Eiffel,[5] Lisp,[6] Lua, Perl,[6] Python,[6] Smalltalk[6]"},
+            {"PHP", "1995", "Rasmus Lerdorf", "Perl, C, C++, Java, Tcl"}
+            };
+ * tableLanguages = new string[] {
+"Javascript", "Python", "Java", "Ruby", "PHP"               
 C++
 CSS
 C#
@@ -54,28 +103,3 @@ F#
 Prelog
 Cuda
 */
-            };
-        public TableViewController (IntPtr handle) : base (handle)
-        {
-        }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
-
-        public override void ViewWillAppear(bool animated)
-        {
-            base.ViewWillAppear(animated);
-
-            TableView.DataSource = new TableViewDataSource(tableLanguages);
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
-        }
-    }
-}
--- ProgramLanguages/ProgramLanguages/TableViewDataSource.cs
+++ ProgramLanguages/ProgramLanguages/TableViewDataSource.cs
@@ -1,41 +1,93 @@
 ﻿using System;
 using UIKit;
 using Foundation; // needed for dataSource inheritance
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ProgramLanguages
 {
     public class TableViewDataSource: UITableViewDataSource
     {
         // there is NO database or storage of Tasks in this example, just an in-memory array
-        string[] tableItems;
+        string[] tableItems;//, detailItems;
+        Dictionary<string, List<string>> indexedTableItems;
+        string[] keys;
+
         string cellIdentifier = "LanguagesCell"; // set in the Storyboard
 
         public TableViewDataSource(string[] items)
         {
-            tableItems = items;
+            ///tableItems = items;
+            indexedTableItems = new Dictionary<string, List<string>>();
+            foreach (var t in items)
+            {
+                if (indexedTableItems.ContainsKey(t[0].ToString()))
+                {
+                    indexedTableItems[t[0].ToString()].Add(t);
+                }
+                else
+                {
+                    indexedTableItems.Add(t[0].ToString(), new List<string>() { t });
+                }
+            }
+            keys = indexedTableItems.Keys.ToArray();
         }
 
+
+        /// <summary> </summary>Called by the TableView to determine how many sections(groups) there are.
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return keys.Length;
+        }
+
+        /// <summary> Called by the TableView to determine how many cells to create for that particular section.
         public override nint RowsInSection(UITableView tableView, nint section)
         {
-            return tableItems.Length;
+            return indexedTableItems[keys[section]].Count;
+            ///return tableItems.Length;
         }
 
+        /// <summary> Sections the index titles.
+        public override String[] SectionIndexTitles(UITableView tableView)
+        {
+            return indexedTableItems.Keys.ToArray();
+        }
+
+        /*
+        /// <summary>
+        /// Called when a row is touched
+        /// </summary>
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            UIAlertController okAlertController = UIAlertController.Create("Row Selected", tableItems[indexPath.Row], UIAlertControllerStyle.Alert);
+            okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+            owner.PresentViewController(okAlertController, true, null);
+
+            tableView.DeselectRow(indexPath, true);
+        }
+        */
+
+        /// <summary> Called by the TableView to get the actual UITableViewCell to render for the particular row
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
             // in a Storyboard, Dequeue will ALWAYS return a cell,
             UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
             // now set the text 
-            //cell.TextLabel.Text = tableItems[indexPath.Row];
-            string item = tableItems[indexPath.Row];
+            if (cell == null) //---- if there are no cells to reuse, create a new one
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier);
+                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, cellIdentifier);
+            }
+            cell.TextLabel.Text = indexedTableItems[keys[indexPath.Section]][indexPath.Row];//tableItems[indexPath.Row];
 
             // Add images to the cell
             //cell.ImageView.Image = UIImage.FromFile("Images/star");
             //cell.ImageView.HighlightedImage = UIImage.FromFile("Images/star2");
-
-            // Cell style should be set to "Subtitle" or "RightDetial" in the storyboard
-            // Add detail information
             /*
+            //Cell style should be set to "Subtitle" or "RightDetial" in the storyboard
+            //Add detail information
+
             const int NUM_DISNEY_DWARVES = 7;
             if (cell.DetailTextLabel != null)
             {
@@ -50,11 +102,7 @@ namespace ProgramLanguages
             }
             
             */
-            //---- if there are no cells to reuse, create a new one
-            if (cell == null)
-            { cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier); }
 
-            cell.TextLabel.Text = item;
             return cell;
         }
 
