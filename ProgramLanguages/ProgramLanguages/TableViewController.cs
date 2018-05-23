2853f32b9af4bd247c5c4bce75b78adf59a9b73c
Cat <catwhitaker@gmail.com>
2018-05-23 12:40:07 -0700

    cleaned comments

--- ProgramLanguages/ProgramLanguages/TableViewController.cs
+++ ProgramLanguages/ProgramLanguages/TableViewController.cs
@@ -7,10 +7,6 @@ namespace ProgramLanguages
 {
     public partial class TableViewController : UITableViewController
     {
-        //public string[] tableLanguages;
-        //List<tableItem> tableItems = new List<tableItem>();
-        //string[] keys;  
-        //Dictionary<string, List<string>> indexedTableItems;
         List<TableItem> tableLanguages = new List<TableItem>();
 
         public TableViewController(IntPtr handle) : base(handle)
@@ -58,71 +54,5 @@ namespace ProgramLanguages
             tableItems.Add (new TableItem("PHP, 1995") { SubHeading="Rasmus Lerdorf"});
             table.Source = new TableSource(tableItems, this);
             Add (table);
-             * 
-             *              private string[,] tableLanguages = new string[5, 4]{
-            //{"Javascript", "Python", "Java", "Ruby", "PHP"};
-            {"Javascript", "1995", "Brendan Eich", "ActionScript, AtScript, CoffeeScript, Dart, JScript .NET, LiveScript, Objective-J, Opa, Perl 6, QML, TypeScript"},
-            {"Python", "1990", "Guido van Rossum", "Boo, Cobra, Coconut, CoffeeScript, D, F#, Falcon, Genie, Go, Groovy, JavaScript, Julia, Nim, Ring, Ruby, Swift"},
-            {"Java", "1995", "James Gosling", "Ada 83, C++, C#, Eiffel, Generic Java, Mesa, Modula-3, Oberon, Objective-C, UCSD Pascal, Object Pascal"},
-            {"Ruby", "1995", "Yukihiro Matsumoto", "Ada,[5] C++,[5] CLU,[6] Dylan,[6] Eiffel,[5] Lisp,[6] Lua, Perl,[6] Python,[6] Smalltalk[6]"},
-            {"PHP", "1995", "Rasmus Lerdorf", "Perl, C, C++, Java, Tcl"}
-            };
-             * 
-             *         
-             private string[,] tableLanguages = new string[5, 4]{
-            //{"Javascript", "Python", "Java", "Ruby", "PHP"};
-            {"Javascript", "1995", "Brendan Eich", "ActionScript, AtScript, CoffeeScript, Dart, JScript .NET, LiveScript, Objective-J, Opa, Perl 6, QML, TypeScript"},
-            {"Python", "1990", "Guido van Rossum", "Boo, Cobra, Coconut, CoffeeScript, D, F#, Falcon, Genie, Go, Groovy, JavaScript, Julia, Nim, Ring, Ruby, Swift"},
-            {"Java", "1995", "James Gosling", "Ada 83, C++, C#, Eiffel, Generic Java, Mesa, Modula-3, Oberon, Objective-C, UCSD Pascal, Object Pascal"},
-            {"Ruby", "1995", "Yukihiro Matsumoto", "Ada,[5] C++,[5] CLU,[6] Dylan,[6] Eiffel,[5] Lisp,[6] Lua, Perl,[6] Python,[6] Smalltalk[6]"},
-            {"PHP", "1995", "Rasmus Lerdorf", "Perl, C, C++, Java, Tcl"}
-            };
- * tableLanguages = new string[] {
-"Javascript", "Python", "Java", "Ruby", "PHP"               
-C++
-CSS
-C#
-GO
-C
-TypeScript
-Shell
-Swift
-Scala
-Objective-C
-Rust
-Elixir
-R
-Perl
-Elm
-Kotlin
-Crystal
-Visual Basic
-Matlab
-Haskell
-
-Groovy
-SQL
-XML
-CoffeeScript
-Groovy
-Clojure
-Arduino
-Lua
-Poweshell
-XSL
-Assembly
-TypeScript
-ActionScript
-Erlang
-ASP
-Dart
-VimL
-Delhi
-OCaml
-ColdFusion
-TeX
-Makefile
-F#
-Prelog
-Cuda
-*/
+             */
+                   
