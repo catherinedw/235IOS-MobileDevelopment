695b1f0c89b7e716606c1fb14a83aab1ce92cf7b
bomnamu77 <bomnamu@gmail.com>
2018-06-01 14:55:23 -0700

    constants struct added

---
+++ TipCalculatorSettings/TipCalculator/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TipCalculator
+{
+    public struct Constants
+    {
+        public const string TIP_PERCENTAGE = "tipPercentageDefault"; //slider
+        public const string TAX_PERCENTAGE = "taxPercentageDefault"; //textField
+        public const string CURRENCY_TYPE = "currencyType"; //make this into an array
+        public const string BACKGROUND_COLOR = "backgroundColor"; //make this into an array of three choices
+    }
+}
